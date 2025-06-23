77061a29c064485e859d7c1c7cae08957724e43d
tjt263 <tjt263@users.noreply.github.com>
2025-06-24 03:13:12 +0800

    Refactored resolve_records() to use dynamic fetcher registry for DNS record types

--- diglet.py
+++ diglet.py
@@ -86,6 +86,17 @@ def fetch_txt(domain, resolver_ip):         return fetch_dns(domain, resolver_ip
 def fetch_uri(domain, resolver_ip):         return fetch_dns(domain, resolver_ip, 'URI')
 def fetch_zonemd(domain, resolver_ip):      return fetch_dns(domain, resolver_ip, 'ZONEMD')
 
+# ---------- Dynamic fetcher registry ----------
+def get_fetchers():
+    return {
+        name.split("fetch_")[1].upper(): func
+        for name, func in globals().items()
+        if callable(func) and name.startswith("fetch_") and name != "fetch_dns"
+    }
+
+FETCHERS = get_fetchers()
+
+# ---------- Resolver ----------
 def resolve_records(domains, resolvers, record_types):
     for i, domain in enumerate(domains):
         resolver_ip = resolvers[i % len(resolvers)]
@@ -93,80 +104,10 @@ def resolve_records(domains, resolvers, record_types):
             "domain": domain,
             "resolver": resolver_ip,
         }
-        if "A" in record_types:
-            record["A"] = fetch_a(domain, resolver_ip)
-        if "AAAA" in record_types:
-            record["AAAA"] = fetch_aaaa(domain, resolver_ip)
-        if "AFSDB" in record_types:
-            record["AFSDB"] = fetch_afsdb(domain, resolver_ip)
-        if "APL" in record_types:
-            record["APL"] = fetch_apl(domain, resolver_ip)
-        if "CAA" in record_types:
-            record["CAA"] = fetch_caa(domain, resolver_ip)
-        if "CDNSKEY" in record_types:
-            record["CDNSKEY"] = fetch_cdnskey(domain, resolver_ip)
-        if "CDS" in record_types:
-            record["CDS"] = fetch_cds(domain, resolver_ip)
-        if "CERT" in record_types:
-            record["CERT"] = fetch_cert(domain, resolver_ip)
-        if "CNAME" in record_types:
-            record["CNAME"] = fetch_cname(domain, resolver_ip)
-        if "DHCID" in record_types:
-            record["DHCID"] = fetch_dhcid(domain, resolver_ip)
-        if "DLV" in record_types:
-            record["DLV"] = fetch_dlv(domain, resolver_ip)
-        if "DNAME" in record_types:
-            record["DNAME"] = fetch_dname(domain, resolver_ip)
-        if "DNSKEY" in record_types:
-            record["DNSKEY"] = fetch_dnskey(domain, resolver_ip)
-        if "DS" in record_types:
-            record["DS"] = fetch_ds(domain, resolver_ip)
-        if "HIP" in record_types:
-            record["HIP"] = fetch_hip(domain, resolver_ip)
-        if "IPSECKEY" in record_types:
-            record["IPSECKEY"] = fetch_ipseckey(domain, resolver_ip)
-        if "KEY" in record_types:
-            record["KEY"] = fetch_key(domain, resolver_ip)
-        if "KX" in record_types:
-            record["KX"] = fetch_kx(domain, resolver_ip)
-        if "LOC" in record_types:
-            record["LOC"] = fetch_loc(domain, resolver_ip)
-        if "MX" in record_types:
-            record["MX"] = fetch_mx(domain, resolver_ip)
-        if "NAPTR" in record_types:
-            record["NAPTR"] = fetch_naptr(domain, resolver_ip)
-        if "NS" in record_types:
-            record["NS"] = fetch_ns(domain, resolver_ip)
-        if "NSEC" in record_types:
-            record["NSEC"] = fetch_nsec(domain, resolver_ip)
-        if "NSEC3" in record_types:
-            record["NSEC3"] = fetch_nsec3(domain, resolver_ip)
-        if "NSEC3PARAM" in record_types:
-            record["NSEC3PARAM"] = fetch_nsec3param(domain, resolver_ip)
-        if "PTR" in record_types:
-            record["PTR"] = fetch_ptr(domain, resolver_ip)
-        if "RP" in record_types:
-            record["RP"] = fetch_rp(domain, resolver_ip)
-        if "RRSIG" in record_types:
-            record["RRSIG"] = fetch_rrsig(domain, resolver_ip)
-        if "SIG" in record_types:
-            record["SIG"] = fetch_sig(domain, resolver_ip)
-        if "SOA" in record_types:
-            record["SOA"] = fetch_soa(domain, resolver_ip)
-        if "SRV" in record_types:
-            record["SRV"] = fetch_srv(domain, resolver_ip)
-        if "SSHFP" in record_types:
-            record["SSHFP"] = fetch_sshfp(domain, resolver_ip)
-        if "TLSA" in record_types:
-            record["TLSA"] = fetch_tlsa(domain, resolver_ip)
-        if "TSIG" in record_types:
-            record["TSIG"] = fetch_tsig(domain, resolver_ip)
-        if "TXT" in record_types:
-            record["TXT"] = fetch_txt(domain, resolver_ip)
-        if "URI" in record_types:
-            record["URI"] = fetch_uri(domain, resolver_ip)
-        if "ZONEMD" in record_types:
-            record["ZONEMD"] = fetch_zonemd(domain, resolver_ip)
+        for rtype in record_types:
+            fetch_func = FETCHERS.get(rtype)
+            if fetch_func:
+                record[rtype] = fetch_func(domain, resolver_ip)
         yield record
 
 # ---------- Printing ----------
